cf80e34c4a71ca30c42c91983446252aa07a0ac1
lshn <liushuning@guazi.com>
2019-07-16 11:59:39 +0800

    支持scss

---
+++ src/components/chat/Chat.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="$style.chat">
+    <div :class="$style.chatHeader">
+      weChat
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Chat"
+    }
+</script>
+
+<style scoped>
+
+</style>
