d750265982a8cd58c8efbfa97cdb6fb86ec7aee3
lshn <liushuning@guazi.com>
2019-07-17 12:00:45 +0800

    chat style

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -3,15 +3,27 @@
     <div class="chatHeader">
       weChat
     </div>
+    <div class="chatContent">
+
+    </div>
+    <div class="chatFooter">
+      <div class="mojiBox">
+        moji
+      </div>
+      <textarea class="textBox" >
+
+      </textarea>
+      <div class="sendBtn">
+        发送
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-    export default {
-      name: 'Chat'
-    }
+  export default {
+    name: 'Chat'
+  }
 </script>
 
-<style scoped>
-
-</style>
+<style lang="scss" src="./index.scss" scoped></style>
