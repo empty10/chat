33220d2da5eee2f8a804e7bc42e12b5f1bb3112d
lshn <liushuning@guazi.com>
2019-07-17 16:59:21 +0800

    chat item

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -4,7 +4,16 @@
       weChat
     </div>
     <div class="chatContent">
-
+      <ul class="messageList">
+        <template  v-for="item in messages">
+          <li class="listItem" :class="{ 'myself':item.from == 'myself' }">
+              <div class="itemContent" :class="{'myselfContent':item.from == 'myself'}">{{item.content}}</div>
+              <div class="itemFace">
+                <img :src="item.portrait" alt="">
+              </div>
+          </li>
+        </template>
+      </ul>
     </div>
     <div class="chatFooter">
       <div class="mojiBox">
@@ -25,8 +34,25 @@
     name: 'Chat',
     data () {
       return {
-        inputValue: ''
-
+        inputValue: '',
+        messages: [
+          {
+            date: '19-07-17 15:06:32',
+            from: 'myself',
+            nickName: 'empty',
+            portrait: 'https://image.guazistatic.com/gz01190717/15/51/c5d5be61ec6032c79c7abc60ced9ed08.jpg',
+            content: 'dfhjdsg132131237653512736812735123521367',
+            location: '北京'
+          },
+          {
+            date: '19-07-17 15:06:32',
+            from: 'others',
+            nickName: 'empty',
+            portrait: 'https://image.guazistatic.com/gz01190717/15/51/c5d5be61ec6032c79c7abc60ced9ed08.jpg',
+            content: 'dfhjdsg132131237653512736812735123521367',
+            location: '北京'
+          }
+        ]
       }
     },
     methods: {
